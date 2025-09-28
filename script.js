278b544c74540c7b32a3e3d84dbc7d8e58826bb9
Futureareavailable743 <stanleyobih57@gmail.com>
2025-09-27 20:07:31 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 
-document.querySelectorAll('.faq-question').forEach(button => {
-    button.addEventListener('click', () => {
-        const answer = button.nextElementSibling;
-        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-    });
-});
-
-
-document.querySelector('.scroll-down-button').addEventListener('click', function(){
-    window.scrollTo({ top: document.querySelector('#next-section').offsetTop,
-behavior: 'smooth'
-    });
-});
